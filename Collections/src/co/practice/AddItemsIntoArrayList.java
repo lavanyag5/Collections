8b9b72987eeefb63507372783191757fcabc4e3a
lavanyag5 <lavanyagovinda1995@gmail.com>
2022-03-11 00:22:43 +0530

    Add Items into ArrayList

---
+++ Collections/src/co/practice/AddItemsIntoArrayList.java
@@ -0,0 +1,20 @@
+package co.practice;
+
+import java.util.ArrayList;
+
+public class AddItemsIntoArrayList {
+
+	public static void main(String[] args) {
+
+		// Creating Arraylist
+		ArrayList<String> list = new ArrayList<>();
+
+		// Adding elements into ArrayList i.e list
+		list.add("item1");
+		list.add("item2");
+		list.add("item3");
+
+		// Printing list elements
+		System.out.println(list);
+	}
+}
